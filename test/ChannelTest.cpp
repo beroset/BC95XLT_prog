0d750752e3f9f32a6823a5e8aead6e7232873f20
Ed Beroset <beroset@ieee.org>
2022-03-06 17:31:20 -0500

    Add more comprehensive tests to ChannelTest suite.

--- test/ChannelTest.cpp
+++ test/ChannelTest.cpp
@@ -12,13 +12,45 @@
 class ChannelTest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(ChannelTest);
     CPPUNIT_TEST(testHumanReadable);
+    CPPUNIT_TEST(testHumanToScannerTranslate);
+    CPPUNIT_TEST(testScannerReadable);
+    CPPUNIT_TEST(testScannerToHumanTranslate);
     CPPUNIT_TEST_SUITE_END();
 public:
     void testHumanReadable() {
         for (const auto& pair : tstchannel) {
             Channel a(pair.human, true);
             std::string result{a.toString(true)};
+            std::cout << pair.human << " => "  << result << "\n";
             CPPUNIT_ASSERT(pair.human == result);
+        }
+    }
+    void testHumanToScannerTranslate() {
+        for (const auto& pair : tstchannel) {
+            Channel a(pair.human, true);
+            std::string result{a.toString(false)};
+            std::cout << pair.human << " => "  << result << "\n";
+            CPPUNIT_ASSERT(pair.scanner == result);
+        }
+    }
+    void testScannerReadable() {
+        for (const auto& pair : tstchannel) {
+            std::stringstream line;
+            line << "RCM" << pair.scanner << '\r';
+            Channel a(line.str(), false);
+            std::string result{a.toString(true)};
+            std::cout << pair.human << " => "  << result << "\n";
+            CPPUNIT_ASSERT(pair.human == result);
+        }
+    }
+    void testScannerToHumanTranslate() {
+        for (const auto& pair : tstchannel) {
+            std::stringstream line;
+            line << "RCM" << pair.scanner << '\r';
+            Channel a(line.str(), false);
+            std::string result{a.toString(false)};
+            std::cout << pair.scanner << " => "  << result << "\n";
+            CPPUNIT_ASSERT(pair.scanner == result);
         }
     }
 
@@ -31,7 +63,11 @@ private:
 };
 
 std::vector<ChannelTest::ChannelPair> ChannelTest::tstchannel{
-    { "21,987.6435,R,S,S", "21,987.6435,R,S,S" }, 
+    { "21,987.6435,R,S,S", "^C21^F987.6435^LR^PS^DS" },
+    { "200,27.1234,S,R,S", "^C200^F27.1234^LS^PR^DS" },
+    { "2,145.2550,R,R,S", "^C2^F145.2550^LR^PR^DS" },
+    { "1,47.1250,R,R,R", "^C1^F47.1250^LR^PR^DR" },
+    { "100,164.5251,S,S,S", "^C100^F164.5251^LS^PS^DS" },
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ChannelTest);
